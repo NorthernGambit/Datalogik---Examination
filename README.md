dd18dc05dd4bedb9ec7bbd6e40f11f77ba8b6292
JonathanAndersson <northerngambit@gmail.com>
2025-12-15 15:42:38 +0100

    v1 av G klar

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Datalogik---Examination
+# Individuell Examination - Introduktion till programmering och datalogiskt tänkande
+
+## G
+
+![Same Directory Image](g.png)
---
+++ exam.js
@@ -0,0 +1,41 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+SET sum = 0
+SET tip = 0
+SET people = 0
+SET share = 0
+
+PRINT "Skriv in Summa: "
+INPUT CONVERT TO NUMBER(sum)
+
+PRINT "Skriv in antal vänner: "
+INPUT CONVERT TO NUMBER(people)
+
+PRINT "Skriv in dricks (I decimalform, 10% blir 0.1): "
+INPUT CONVERT TO NUMBER(tip)
+
+share = (sum + sum * 0.1) / people
+PRINT "Pris per person inklusive dricks är: " + share
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+SET variabel startOrd till "FOUR";
+SET variabel slutOrd till "FIVE";
+
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+    
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
